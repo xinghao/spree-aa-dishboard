5975297f0f343fa2e0fa909df3ba3dfe50388d56
xinghao <Xinghao@airarena.net>
2012-07-05 12:22:20 +1000

    add city to old orders sheets

--- app/controllers/spree/aa/shipmentmg_controller.rb
+++ app/controllers/spree/aa/shipmentmg_controller.rb
@@ -47,7 +47,7 @@ class Spree::Aa::ShipmentmgController < Spree::Admin::BaseController
     orders = Spree::Order.includes(:ship_address, :inventory_units, :line_items, :user).where("state = 'complete' and shipment_state != 'shipped'").order("completed_at asc").all
     
     csv_string = CSV.generate do |csv|
-      csv << ["order number", "date", "payment_state", "shipment_state", "name", "email", "street address", "state", "postcode", "phone", "products"]
+      csv << ["order number", "date", "payment_state", "shipment_state", "name", "email", "street address", "suburb", "state", "postcode", "phone", "products"]
       
       orders.each do |order|
         address1 = order.ship_address.address1
@@ -63,7 +63,7 @@ class Spree::Aa::ShipmentmgController < Spree::Admin::BaseController
           end
         end
         address1 = address1 + " " + address2 if !address2.nil? && address2.size > 0        
-        csv << [order.number, order.completed_at, order.payment_state, order.shipment_state, order.ship_address.full_name, order.user.email, address1, order.ship_address.state_text, order.ship_address.zipcode, order.ship_address.phone, products]  
+        csv << [order.number, order.completed_at, order.payment_state, order.shipment_state, order.ship_address.full_name, order.user.email, address1, order.ship_address.city, order.ship_address.state_text, order.ship_address.zipcode, order.ship_address.phone, products]  
       end
     end 
     
