3fd7626260626a852139724b2d89e4876fcb8e51
xinghao <Xinghao@airarena.net>
2012-07-11 13:17:53 +1000

    show all the werid orders in dashboard page

--- app/controllers/spree/aa/dashboard_controller.rb
+++ app/controllers/spree/aa/dashboard_controller.rb
@@ -4,7 +4,7 @@ module Spree
     class DashboardController < Spree::Admin::BaseController
       def index
          
-        @orders = Spree::Order.where("state = 'complete' and payment_state != 'paid'").order("completed_at asc").all 
+        @orders = Spree::Order.where("(state = 'complete' and payment_state != 'paid') or (state != 'complete' and state != 'cart' and state != 'address'  and  state != 'payment' and state != 'delivery' and state != 'confirm' and state != 'canceled' and state != 'returned' and state != 'awaiting_return' and state != 'resumed')").order("completed_at asc").all 
         
         pp = Spree::Property.find_by_name("popularity");
         @products = Spree::Product.includes(:properties).where("spree_product_properties.property_id = ? and deleted_at is null ", pp.id).order("value desc, spree_products.id desc");                 
